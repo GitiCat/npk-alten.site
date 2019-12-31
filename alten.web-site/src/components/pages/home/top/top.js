3f0370351a9f7b4be809bd7de5b44d55e26a92ed
Ivan Zhuravlev <ivan_zhuravlev_1997@mail.ru>
2019-12-31 11:26:41 +0300

    q

--- alten.web-site/src/components/pages/home/top/top.js
+++ alten.web-site/src/components/pages/home/top/top.js
@@ -1,9 +1,11 @@
 import React from "react";
+import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 
 export default function Header() {
   return (
     <div className="top d-flex flex-dir--row row-vp-center row-hp-start">
       <div className="top_bg-image abs-fscreen"></div>
+      <div className="t-button"><div><ArrowDropDownIcon/></div></div>
       <div className="top_content article__title">
         <h1>
 					Акционерное общество
